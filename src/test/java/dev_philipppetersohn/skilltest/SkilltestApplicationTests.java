25b5867e1593fed9509866ac944ad90fee9bdece
philpetersohn <petersohn.phil@gmail.com>
2024-10-12 17:31:00 -0700

    Setup project

---
+++ src/test/java/dev_philipppetersohn/skilltest/SkilltestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev_philipppetersohn.skilltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
